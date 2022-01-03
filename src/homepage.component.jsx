c78b2b67f46d6dc887ac874c7f4a8502124a8f75
Khojiakbar-007 <xxomniknightxoji@gmail.com>
2022-01-02 18:32:02 -0800

    Initial commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./homepage-styles/homepage.styles.css";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h2>HATS</h2>
+          <span>SHOP NOW</span>
+        </div>
+      </div>
+
+      <h3 className="">
+        Something TO ChEcK Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Tenetur ad nobis praesentium id fuga, necessitatibus nihil veniam,
+        natus ratione dignissimos officiis fugit doloribus omnis eum. A debitis
+        necessitatibus et id!
+      </h3>
+
+      <div className="menu-item">
+        <div className="content">
+          <h2>JACKETS</h2>
+          <span>SHOP NOW</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h2>SNEAKERS</h2>
+          <span>SHOP NOW</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h2>WOMENS</h2>
+          <span>SHOP NOW</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h2>MANS</h2>
+          <span>SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
