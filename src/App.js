94b41d68b330af0a753f7cb213f7029458bd67e3
Khojiakbar-007 <xxomniknightxoji@gmail.com>
2022-01-16 11:44:21 -0800

    Added shop page, components: collection-preview and collection-item

--- src/App.js
+++ src/App.js
@@ -3,32 +3,14 @@ import { Route } from "react-router-dom";
 
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  );
-};
-
-const PageWithID = () => (
-  <div>
-    <h1>Page with ID</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
-      {/* <Switch> */}
       <Route exact={true} path={"/"} component={HomePage}></Route>
 
-      <Route path={"/shop/hats"} component={HatsPage}></Route>
-
-      <Route path={"/shop/hats/:something"} component={PageWithID}></Route>
-      {/* </Switch> */}
+      <Route exact path={"/shop/"} component={ShopPage}></Route>
     </div>
   );
 }
